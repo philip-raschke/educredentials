ea5d292b3d99e65d3b1cfcd018f827908d4167ca
Jonas Krause <103564990+JonasKrau@users.noreply.github.com>
2023-05-12 11:06:07 +0200

    Add files via upload

---
+++ badge_data/version.php
@@ -0,0 +1,5 @@
+<?php
+
+$plugin->version = 2022050100; // Versionsnummer
+$plugin->requires = 2020092500; // Mindestanforderung an Moodle-Version
+$plugin->component = 'local_badge_data'; // Name des Plugins
