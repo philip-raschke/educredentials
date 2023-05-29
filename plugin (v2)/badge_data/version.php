1bf92685b306d7354e03b17b13dd93cac2a9ebef
Jonas Krause <103564990+JonasKrau@users.noreply.github.com>
2023-05-29 21:15:08 +0200

    Delete plugin (v2)/badge_data directory

--- plugin (v2)/badge_data/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$plugin->version = 2022050100; // Versionsnummer
-$plugin->requires = 2020092500; // Mindestanforderung an Moodle-Version
-$plugin->component = 'local_badge_data'; // Name des Plugins
