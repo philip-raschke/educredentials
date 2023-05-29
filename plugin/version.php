3f8173f303b00bc67747fb2e3d40c596a33f1037
Jonas Krause <103564990+JonasKrau@users.noreply.github.com>
2023-05-29 21:14:53 +0200

    Delete plugin directory

--- plugin/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$plugin->version = 2022050100; // Versionsnummer
-$plugin->requires = 2020092500; // Mindestanforderung an Moodle-Version
-$plugin->component = 'plugin_badge_json'; // Name des Plugins
