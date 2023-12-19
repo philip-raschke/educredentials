90f3d6a4be3333184a4a8a146b59b2b405a9576e
Jonas Krause <103564990+JonasKrau@users.noreply.github.com>
2023-12-19 15:20:45 +0100

    Implemented all Moodle Guidline Policies

--- badge_data/db/uninstall.php
+++ badge_data/db/uninstall.php
@@ -1,19 +1,35 @@
 <?php
+
 // Load Moodle core
+// 1. Load the Moodle core. This is essential to access all Moodle functionalities and configurations.
 require_once(dirname(__FILE__) . '/../../../config.php');
 
-function xmldb_local_badge_data_uninstall(){
-    copy('C:\wamp64\www\moodle4\local\badge_data\original_renderer.php','C:\wamp64\www\moodle4\badges\renderer.php');
-}
-
-/*
-
-function xmldb_local_yourpluginname_uninstall() {
+function xmldb_local_badge_data_uninstall() {
+    // 2. Declare the global configuration variable $CFG to access global Moodle configurations.
     global $CFG;
 
-    // Restore original file
-    $original_file = file_get_contents($CFG->dirroot . '/badges/renderer.php');
-    file_put_contents($CFG->dirroot . 'local/badge_data/renderer.php', $original_file);
-}
+    // 3. Define the base path for the Moodle directory to enable relative path specification.
+    //    This approach ensures that the code is server-independent and more portable.
+    $moodleBasePath = $CFG->dirroot;
 
-*/
+    // 4. Set the paths for the required file operations relative to the Moodle base path.
+    //    a. Path to the backup of the original 'renderer.php' in the plugin directory.
+    //       This file was saved during the plugin installation as a backup.
+    $backupRendererPath = $moodleBasePath . '/local/badge_data/original_renderer.php';
+    //    b. Path to the 'renderer.php' in the Moodle 'badges' directory.
+    //       This is where the original renderer file will be restored from the backup.
+    $originalRendererPath = $moodleBasePath . '/badges/renderer.php';
+
+    // 5. Perform the file operation with error checking.
+    //    a. Copy the original 'renderer.php' back from the backup location.
+    //       This step is crucial to restore the original state of the 'renderer.php' file upon uninstallation of the plugin.
+    //       Logging errors helps in diagnosing issues during the uninstallation process.
+    if (!copy($backupRendererPath, $originalRendererPath)) {
+        error_log('Error restoring the original renderer file');
+        return false;
+    }
+
+    // 6. Return true to indicate a successful restoration and completion of the uninstallation process.
+    return true;
+}
+?>
